bbe783adb2b25c3d401476016856a24d6db57b8d
nharsch <nigelharsch@gmail.com>
2016-03-26 22:31:49 -0400

    that's how we do imports

---
+++ src/clojure_noob/notes.clj
@@ -0,0 +1,107 @@
+(:require src/clojure-noob/core.clj)
+
+(error-message :mild)
+(if true
+  "By Agthar's hammer!"
+  "nope")
+
+(if false
+  "By Odin's Elbow!")
+
+(if true
+  (do (println "Success!")
+      "By Zs hammer")
+  (do (println "Failure!")
+      "By A's trident"))
+
+(when true
+  (println "Success!")
+  "abra cadabra")
+
+(def failed-hero-names
+  ["Larry Potter" "Doreen" "Bulk"])
+
+(def actual-names
+  ["nigel" "bethany"])
+
+(=
+  (nth actual-names 1)
+  "bethany")
+
+(defn error-message
+  [severity]
+  (str "OH GOD! DISASTER! WE'RE "
+       (if (= severity :mild)
+         "MILDLY INCONVENIENCED!"
+         "DOOMED")))
+
+(error-message :mild)
+
+; maps
+(=
+  (get {:a 0 :b 1} :n)
+  nil)
+(=
+  (get {:a 0 :b 1} :n 3)
+  3)
+
+; get-in
+(=
+  (get-in {:a 0 :b {:c "ho hum"}} [:b :c])
+  "ho hum")
+
+; treat map like a function
+(=
+  ({:name "The Human Coffeepot"} :name)
+  "The Human Coffeepot")
+
+; keywords as functions
+(=
+ (:a {:a 1 :b 2 :c 3})
+ 1)
+
+(=
+ (:d {:a 1 :b 2} "some other")
+ "some other")
+
+; Vectors
+[3 2 1]
+
+(=
+  (get [3 2 1] 0)
+  3)
+
+(
+ (get ["a" {:name "pugs"}] 1)
+ :name)
+
+(=
+  (vector "creepy" "full" "moon")
+  ["creepy" "full" "moon"])
+
+; conj
+(=
+ (conj [1 2 3] 4)
+ [1 2 3 4])
+
+; lists
+'(1 2 3 4)
+(nth '(1 2 3 4) 0)
+
+(=
+ (list 1 "two" {3 4})
+ '(1 "two" {3 4}))
+
+; conj puts item at front
+(conj
+  '(1 2 3) 4)
+
+(=
+ (hash-set 1 1 2 2)
+ #{1 2})
+
+
+
+
+
+ 
